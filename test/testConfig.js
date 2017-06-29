107b2fb25973526ee184dc8f662a1a87a11f737b
Christos Paraskeva <ChristosMAC@Christoss-MacBook-2008.local>
2017-06-29 10:07:42 +0100

    Sets up test environment

---
+++ test/testConfig.js
@@ -0,0 +1,6 @@
+global.assert = require('assert');
+global.chai = require("chai");
+global.expect = require('chai').expect;
+global.sinon = require('sinon');
+global.sinonChai = require("sinon-chai");
+chai.use(sinonChai);
